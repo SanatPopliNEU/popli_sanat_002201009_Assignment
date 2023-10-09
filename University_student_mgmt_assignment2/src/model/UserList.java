f9c7ff364006d0b15a0b497dd3bff642713c4bba
SanatPopliNEU <144727527+SanatPopliNEU@users.noreply.github.com>
2023-10-08 20:24:41 -0400

    Add files via upload

---
+++ University_student_mgmt_assignment2/src/model/UserList.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author Jayesh Chindarkar
+ */
+public class UserList {
+    
+   
+    ArrayList<User> userList = null;
+    
+     public UserList(){
+    
+        this.userList = new ArrayList<User>();
+        User user1 = new User();
+        user1.setNuId(null);
+        user1.setUserId("admin");
+        user1.setUserName("admin");
+        user1.setPassword("admin");
+        user1.setEnabled("YES");
+        userList.add(user1);
+        System.out.print(userList);
+    }
+    
+
+    public ArrayList<User> getUser() {
+        return userList;
+    }
+
+    public void setUser(ArrayList<User> userList) {
+        this.userList = userList;
+    }
+    
+    public User addUser(User user){
+    
+       // User user = new User();
+        userList.add(user);
+        return user;
+    }
+    
+    public void deleteUser(User user){    
+        userList.remove(user);
+    } 
+    
+    
+   public User searchUser(String nueId){
+    String nuId = nueId;
+       for(User u: userList){
+           if(nuId.equalsIgnoreCase(u.getNuId())){
+           return u;
+           }
+       }
+   return null;
+   }
+   
+   public User searchUserByUserId(String userId){
+    String nuId = userId;
+       for(User u: userList){
+           if(nuId.equalsIgnoreCase(u.getUserId())){
+           return u;
+           }
+       }
+   return null;
+   }
+    
+}
